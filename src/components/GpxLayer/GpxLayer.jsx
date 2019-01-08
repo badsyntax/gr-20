21c681d01b458d49167a7fd589ab6fb824c34de8
Richard Willis <willis.rh@gmail.com>
2019-01-08 21:05:46 +0100

    Now using custom GPX source with start and end points

--- src/components/GpxLayer/GpxLayer.jsx
+++ src/components/GpxLayer/GpxLayer.jsx
@@ -9,7 +9,10 @@ import Fill from "ol/style/Fill";
 import Stroke from "ol/style/Stroke";
 import Style from "ol/style/Style";
 
-import GPX from "ol/format/GPX";
+import { fromLonLat } from "ol/proj";
+import MultiLineString from "ol/geom/MultiLineString";
+import Point from "ol/geom/Point";
+import Feature from "ol/Feature";
 import ElevationProfile from "../ElevationProfile/ElevationProfile";
 
 const style = {
@@ -23,18 +26,6 @@ const style = {
         color: "#ff0",
         width: 1
       })
-    })
-  }),
-  LineString: new Style({
-    stroke: new Stroke({
-      // color: '#000',
-      width: 3
-    })
-  }),
-  MultiLineString: new Style({
-    stroke: new Stroke({
-      color: "rgba(0,60,136)",
-      width: 4
     })
   })
 };
@@ -66,27 +57,173 @@ class GpxLayer extends Component {
     }
   }
 
-  setSource() {
+  async setSource() {
     const { onSourceChange, gpxUrl } = this.props;
 
-    const source = new VectorSource({
-      url: gpxUrl,
-      format: new GPX()
-    });
-    this.vectorLayer.setSource(source);
+    onSourceChange(false);
     this.setState({
-      source,
       sourceLoaded: false
     });
 
-    onSourceChange(false);
-    source.once("change", evt => {
-      if (evt.target.getState() === "ready") {
-        onSourceChange(true);
-        this.setState({
-          sourceLoaded: true
-        });
-      }
+    const vectorSource = new VectorSource();
+
+    const fetchStart = window.performance.now();
+    const response = await fetch(gpxUrl);
+    const responseText = await response.text();
+
+    console.log(
+      "It took %dms to fetch the GPX track",
+      window.performance.now() - fetchStart
+    );
+
+    const xmlParseStart = window.performance.now();
+
+    const responseDoc = new DOMParser().parseFromString(
+      responseText,
+      "application/xml"
+    );
+
+    const trk = responseDoc.getElementsByTagName("trk")[0];
+    const trkseg = trk.getElementsByTagName("trkseg")[0];
+    const trkpt = trkseg.getElementsByTagName("trkpt");
+    const wpt = responseDoc.getElementsByTagName("wpt");
+    const metadata = responseDoc.getElementsByTagName("metadata")[0];
+    const trackName = metadata
+      .getElementsByTagName("name")[0]
+      .textContent.trim();
+
+    // console.log("wpt", wpt);
+    [...wpt].forEach(wayPoint => {
+      const lon = parseFloat(wayPoint.getAttribute("lon"));
+      const lat = parseFloat(wayPoint.getAttribute("lat"));
+      const ele = parseFloat(
+        wayPoint.getElementsByTagName("ele")[0].textContent.trim()
+      );
+      const name = wayPoint.getElementsByTagName("name")[0].textContent.trim();
+      const wayPointGeom = new Point(fromLonLat([lon, lat, ele]));
+
+      const wayPointFeature = new Feature({
+        geometry: wayPointGeom,
+        name
+      });
+
+      wayPointFeature.setStyle(
+        new Style({
+          image: new Circle({
+            fill: new Fill({
+              color: "yellow"
+            }),
+            radius: 5,
+            stroke: new Stroke({
+              color: "#ff0",
+              width: 1
+            })
+          })
+        })
+      );
+
+      vectorSource.addFeature(wayPointFeature);
+    });
+
+    const coords = [...trkpt].map(point => [
+      parseFloat(point.getAttribute("lon")),
+      parseFloat(point.getAttribute("lat")),
+      parseFloat(point.getElementsByTagName("ele")[0].textContent.trim())
+    ]);
+
+    const startPointGeom = new Point(fromLonLat(coords[0]));
+    const endPointGeom = new Point(fromLonLat(coords[coords.length - 1]));
+
+    const sampleSize = 1;
+    const sampedCoords = coords.filter(
+      (coord, index) => index % sampleSize === 0
+    );
+
+    const points = sampedCoords.map((coord, index) =>
+      sampedCoords.slice(index, index + 2)
+    );
+
+    console.log(
+      "It took %dms to parse and sample the GPX track (%d data points)",
+      window.performance.now() - xmlParseStart,
+      points.length
+    );
+
+    const multiLineStart = window.performance.now();
+    const multiline = new MultiLineString(points).transform(
+      "EPSG:4326",
+      "EPSG:3857"
+    );
+
+    const multiLineFeature = new Feature({
+      geometry: multiline,
+      name: trackName
+    });
+
+    multiLineFeature.setStyle(
+      new Style({
+        stroke: new Stroke({
+          color: "rgba(0,60,136)",
+          width: 4
+        })
+      })
+    );
+
+    const startPointFeature = new Feature({
+      geometry: startPointGeom,
+      name: "Start Point"
+    });
+
+    startPointFeature.setStyle(
+      new Style({
+        image: new Circle({
+          fill: new Fill({
+            color: "green"
+          }),
+          radius: 5,
+          stroke: new Stroke({
+            color: "#ff0",
+            width: 1
+          })
+        })
+      })
+    );
+
+    const endPointFeature = new Feature({
+      geometry: endPointGeom,
+      name: "End Point"
+    });
+
+    endPointFeature.setStyle(
+      new Style({
+        image: new Circle({
+          fill: new Fill({
+            color: "red"
+          }),
+          radius: 5,
+          stroke: new Stroke({
+            color: "#ff0",
+            width: 1
+          })
+        })
+      })
+    );
+
+    vectorSource.addFeature(multiLineFeature);
+    vectorSource.addFeature(startPointFeature);
+    vectorSource.addFeature(endPointFeature);
+
+    this.vectorLayer.setSource(vectorSource);
+
+    console.log(
+      "It took %dms to init the MultiLineString",
+      window.performance.now() - multiLineStart
+    );
+
+    onSourceChange(true);
+    this.setState({
+      sourceLoaded: true,
+      source: vectorSource
     });
   }
 
