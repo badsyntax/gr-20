d0d04d608bce24abfc886156013b7e63b35337f8
Richard Willis <willis.rh@gmail.com>
2019-01-19 14:35:48 +0100

    Re-enable react-snap

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,14 @@ import { hydrate, render } from 'react-dom';
 import './index.scss';
 import App from './components/App/App';
 
+window.snapSaveState = () => {
+  // Remove all elements created by OpenLayers. This is DOM not managed by React, so
+  // React cannot hydrate it.
+  document
+    .querySelectorAll('[class^="ol"],canvas')
+    .forEach(element => element.parentNode.removeChild(element));
+};
+
 const rootElement = document.getElementById('root');
 
 if (rootElement.hasChildNodes()) {
