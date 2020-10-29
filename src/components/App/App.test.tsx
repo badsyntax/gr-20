c2898201d989544cd117e9eef5835d830df4bc9a
Richard Willis <willis.rh@gmail.com>
2020-10-29 22:57:08 +0000

    Fix tests

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
+import configureMockStore from 'redux-mock-store';
 import ReactDOM from 'react-dom';
 import { HelmetProvider } from 'react-helmet-async';
+import { Provider } from 'react-redux';
 import App from './App';
 
+const mockStore = configureMockStore();
+
 it('renders without crashing', () => {
+  const store = mockStore({
+    spinner: {
+      isVisible: false,
+    },
+  });
   const div = document.createElement('div');
   ReactDOM.render(
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>,
+    <Provider store={store}>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </Provider>,
     div
   );
   ReactDOM.unmountComponentAtNode(div);
